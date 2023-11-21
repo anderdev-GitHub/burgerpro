6a8d19f7120b72a3e065401ea4d346b9a292bac1
anderdev-GitHub <andersondiasleite@icloud.com>
2023-11-21 18:42:30 -0300

    finalizado logica de filter

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
-const list = document.querySelector('ul');
-const buttonShowAll = document.querySelector('.show-all');
-const buttonMapAll = document.querySelector('.map-all');
+const list = document.querySelector("ul");
+const buttonShowAll = document.querySelector(".show-all");
+const buttonMapAll = document.querySelector(".map-all");
+const buttonSumAll = document.querySelector(".sum-all");
+const buttonFilterAll = document.querySelector(".filter-all");
+
+const formatCurrency = (value) => {
+  const newValue = value.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return newValue;
+};
 
 const showAll = (productsArray) => {
-    let myLi = ''
+  let myLi = "";
 
-    productsArray.forEach((product) => {
-        myLi += `
+  productsArray.forEach((product) => {
+    myLi += `
             <li>
                 <img class="hamburgueres" src=${product.src}>
                 <p>${product.name}</p>
-                <p class="hamburguer-price">R$ ${product.price}</p>
+                <p class="hamburguer-price"> ${formatCurrency(
+                  product.price
+                )}</p>
             </li>
-            `
-    });
+            `;
+  });
 
-    list.innerHTML = myLi
-}
-
-const mapAllItems = () => {
-    const newPrices = menuOptions.map((product) => ({
-        ...product,
-        price: product.price * 0.9, // aplicando 10% desconto
-    }));
-
-    showAll(newPrices)
+  list.innerHTML = myLi;
 };
 
-buttonShowAll.addEventListener('click', () => showAll(menuOptions));
-buttonMapAll.addEventListener('click', mapAllItems);
+const mapAllItems = () => {
+  const newPrices = menuOptions.map((product) => ({
+    ...product,
+    price: product.price * 0.9, // aplicando 10% desconto
+  }));
+
+  showAll(newPrices);
+};
+
+const sumAllItems = () => {
+  const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
+
+  list.innerHTML = `
+            <li>
+                <p>O valor total de todos os itens é ${formatCurrency(
+                  totalValue
+                )}</p>
+            </li>
+            `;
+};
+
+const filterAllItems = () => {
+  const filterJustVegan = menuOptions.filter((product) => product.vegan);
+
+  showAll(filterJustVegan);
+};
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions));
+buttonMapAll.addEventListener("click", mapAllItems);
+buttonSumAll.addEventListener("click", sumAllItems);
+buttonFilterAll.addEventListener("click", filterAllItems);
