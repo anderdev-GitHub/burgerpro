49c4e41ee63b7742ec010b7072939b88385b06eb
anderdev-GitHub <andersondiasleite@icloud.com>
2023-11-21 16:38:00 -0300

    finalizado logica do map

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const list = document.querySelector('ul');
 const buttonShowAll = document.querySelector('.show-all');
+const buttonMapAll = document.querySelector('.map-all');
 
-let myLi = ''
+const showAll = (productsArray) => {
+    let myLi = ''
 
-const showAll = () => {
-    menuOptions.forEach((product) => {
+    productsArray.forEach((product) => {
         myLi += `
             <li>
                 <img class="hamburgueres" src=${product.src}>
@@ -17,4 +18,14 @@ const showAll = () => {
     list.innerHTML = myLi
 }
 
-buttonShowAll.addEventListener('click', showAll);
+const mapAllItems = () => {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9, // aplicando 10% desconto
+    }));
+
+    showAll(newPrices)
+};
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions));
+buttonMapAll.addEventListener('click', mapAllItems);
